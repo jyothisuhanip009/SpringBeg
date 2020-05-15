a753f8e096abdcd1fc96387a3b30136c58831639
Jyothi Suhani <jyothisuhanip009@gmail.com>
2020-05-15 15:49:45 +0530

    BeanwithProperty

---
+++ FirstSpringProject/src/com/example/springstarter/Restaurant.java
@@ -0,0 +1,19 @@
+package com.example.springstarter;
+
+public class Restaurant {
+	
+	String welcomeNote;
+	
+	public String getWelcomeNote() {
+		return welcomeNote;
+	}
+
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
+	}
+
+	public void greetCustomer() {
+
+		System.out.println(welcomeNote);
+	}
+}
---
+++ FirstSpringProject/src/com/example/springstarter/TestSpringProject.java
@@ -0,0 +1,13 @@
+package com.example.springstarter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpringProject {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+ 
+		Restaurant restaurantObj = (Restaurant) context.getBean("restaurantBean");
+		restaurantObj.greetCustomer();
+	}
+}
