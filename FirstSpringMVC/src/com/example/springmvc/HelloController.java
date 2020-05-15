eb2eea16d69cae31493c5aeacea2913a71bf6525
Jyothi Suhani <jyothisuhanip009@gmail.com>
2020-05-16 00:26:46 +0530

    request mapping at class level

--- FirstSpringMVC/src/com/example/springmvc/HelloController.java
+++ FirstSpringMVC/src/com/example/springmvc/HelloController.java
@@ -8,13 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/welcome")
 public class HelloController {
 
-	@RequestMapping("/welcome")
+	@RequestMapping("/hello")
 	public ModelAndView helloWorld() {
 
 		ModelAndView model = new ModelAndView("HelloPage");
 		model.addObject("msg","hello world");
+
+		return model;
+	}
+	
+	@RequestMapping("/hi")
+	public ModelAndView hiWorld() {
+
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("msg","hi world");
 
 		return model;
 	}
